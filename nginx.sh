72a4f7630b69ebec2f3e59b70d739343426fa20f
pooja-mittal068 <pooja.mittal068@gmail.com>
2022-12-01 16:07:14 +0530

    nginx site available directory

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf mysite.conf /etc/nginx/sites-available/app
+sudo cp -rf mysite.conf /etc/nginx/sites-available/mysite
 chmod 710 /var/lib/jenkins/workspace/django-cicd-jenkins
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/mysite /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
