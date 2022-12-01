c6d6f121333913a0281a2daeb562e7324a17d725
pooja-mittal068 <pooja.mittal068@gmail.com>
2022-12-01 15:02:44 +0530

    setup nginx

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '9tf$jps6u-rxnv8nuur=*z&44$d!*_k@9td4jfaurtd5)xu_50'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
